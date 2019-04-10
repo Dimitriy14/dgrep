a7d1e76f5c798168e35a1d698d5bf9441827b1de
Dimitriy14 <yankovskiydy98@gmail.com>
2019-04-10 15:56:07 +0300

    Added some concurency magic

--- dgrep.go
+++ dgrep.go
@@ -16,15 +16,9 @@ var (
 	contextLines = flag.Int("c", 3, "number of context lines")
 	ignoreCase   = flag.Bool("i", false, "ignore case")
 	pattern      = flag.String("type", "go", "file extension")
-
-	wg sync.WaitGroup
 )
 
-type jobRequest struct {
-	path string
-	info os.FileInfo
-}
-
+//Results is a result of parsing
 type Results struct {
 	path  string
 	lines string
@@ -41,90 +35,136 @@ func main() {
 		regx = "(?-i)" + os.Args[len(os.Args)-2]
 	}
 
-	reqch := make(chan jobRequest)
-	done := make(chan struct{})
-
-	numberOfJob := 3
-
-	for i := 0; i < numberOfJob; i++ {
-		go parseFile(i, reqch, done)
-	}
-
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.Mode().IsRegular() {
-			if filepath.Ext(info.Name()) == "."+*pattern {
-				reqch <- jobRequest{path, info}
-			}
-		}
-		return nil
-	})
+	results, err := worker(root)
 
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
-	for i := 0; i < numberOfJob; i++ {
-		done <- struct{}{}
-	}
+	for _, r := range results {
 
+		fmt.Printf("%s:\n%s", r.path, r.lines)
+	}
 }
 
-func parseFile(id int, request chan jobRequest, done chan struct{}) {
-	for {
-		select {
-		case <-done:
-			fmt.Printf("Routine №:%d is finished\n", id)
-			return
-		case req := <-request:
-			file, err := os.Open(req.path)
+func worker(root string) ([]Results, error) {
+	done := make(chan struct{})
+	defer close(done)
 
+	paths, errc := walkFiles(done, root)
+
+	resch := make(chan Results)
+
+	var wg sync.WaitGroup
+
+	numGoroutine := 5
+
+	wg.Add(numGoroutine)
+
+	for i := 0; i < numGoroutine; i++ {
+		go func() {
+			parseFile(paths, resch, done)
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(resch)
+	}()
+
+	var results []Results
+
+	for r := range resch {
+		results = append(results, r)
+	}
+
+	if err := <-errc; err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func walkFiles(done chan struct{}, root string) (chan string, chan error) {
+	paths := make(chan string)
+	errc := make(chan error, 1)
+
+	go func() {
+		defer close(paths)
+
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				log.Println("file opening err:", err)
-				return
+				return err
 			}
 
-			fileScanner := bufio.NewScanner(file)
-
-			re := regexp.MustCompile(regx)
-
-			var lineNumber int
-			var context string
-			var contextLineCounter int
-			var mathedLines string
-
-			fileScanner.Scan()
-
-			for {
-				lineNumber++
-				contextLineCounter++
-
-				context += fmt.Sprintf("%d: %s\n", lineNumber, fileScanner.Text())
-
-				if contextLineCounter == *contextLines {
-					if re.FindString(context) != "" {
-						mathedLines += context
+			if info.Mode().IsRegular() {
+				if filepath.Ext(info.Name()) == "."+*pattern {
+					select {
+					case paths <- path:
+					case <-done:
+						return fmt.Errorf("walk canceled")
 					}
-
-					context = "\n"
-					contextLineCounter = 0
-				}
-
-				if !fileScanner.Scan() {
-					if re.FindString(context) != "" {
-						mathedLines += context
-					}
-
-					break
 				}
 			}
 
-			if mathedLines != "" {
-				fmt.Printf("\n%s: \t%s", req.path, mathedLines)
+			return nil
+		})
+	}()
+
+	return paths, errc
+}
+
+func parseFile(paths chan string, res chan Results, done chan struct{}) {
+	for path := range paths {
+		file, err := os.Open(path)
+
+		if err != nil {
+			log.Println("file opening err:", err)
+			return
+		}
+
+		defer file.Close()
+
+		fileScanner := bufio.NewScanner(file)
+
+		re := regexp.MustCompile(regx)
+
+		var lineNumber int
+		var context string
+		var contextLineCounter int
+		var mathedLines string
+
+		fileScanner.Scan()
+
+		for {
+			lineNumber++
+			contextLineCounter++
+
+			context += fmt.Sprintf("%d: %s\n", lineNumber, fileScanner.Text())
+
+			if contextLineCounter == *contextLines {
+				if re.FindString(context) != "" {
+					mathedLines += context
+				}
+
+				context = "\n"
+				contextLineCounter = 0
 			}
+
+			if !fileScanner.Scan() {
+				if re.FindString(context) != "" {
+					mathedLines += context
+				}
+
+				break
+			}
+		}
+
+		select {
+		case res <- Results{path, mathedLines}:
+		case <-done:
+			return
 		}
 	}
 }
